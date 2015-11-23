bb0d008e94b81c1fb556f88d2f913aee9722878c
staffan <staffan.arvidsson@gmail.com>
2015-11-23 17:14:30 +0100

    Added Serializer for LibLibAlg, beginning coding for partitioning data into test/training-sets for liblin.

--- cp/src/main/scala/se/uu/farmbio/cp/alg/LIBLINEAR.scala
+++ cp/src/main/scala/se/uu/farmbio/cp/alg/LIBLINEAR.scala
@@ -5,7 +5,6 @@ import org.apache.spark.mllib.linalg.Vector
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
-
 import de.bwaldvogel.liblinear.Feature
 import de.bwaldvogel.liblinear.FeatureNode
 import de.bwaldvogel.liblinear.Linear
@@ -13,19 +12,34 @@ import de.bwaldvogel.liblinear.Parameter
 import de.bwaldvogel.liblinear.Problem
 import de.bwaldvogel.liblinear.SolverType
 import se.uu.farmbio.cp.UnderlyingAlgorithm
+import se.uu.farmbio.cp.UnderlyingAlgorithmSerializer
+import se.uu.farmbio.cp.AggregatedICPClassifier
+import org.apache.spark.SparkContext
+import scala.util.Random
+import se.uu.farmbio.cp.ICP
+
 
 class LibLinAlg(
+  
   private var training: Array[LabeledPoint],
   private val solverType: SolverType,
   private val regParam: Double,
-  private val tol: Double)
+  private val tol: Double,
+  private var model: SVMModel = null)
   extends UnderlyingAlgorithm(null.asInstanceOf[RDD[LabeledPoint]]) {
-
+  
+  def this(model: SVMModel){
+    this(null, null, 0.0, 0.0, model);
+  }
+  
+  //private var model: SVMModel = _
+  def getModel = if(model!=null) model else throw new IllegalAccessException("Illegal access of model (not been created yet)");
+  
   override def trainingProcedure(nullRdd: RDD[LabeledPoint]) = {
     val liblinModel = LIBLINEAR.train(training, solverType, regParam, tol)
     training = null
     val weights = liblinModel.getFeatureWeights
-    val model = new SVMModel(Vectors.dense(weights).toSparse, 0.0)
+    model = new SVMModel(Vectors.dense(weights).toSparse, 0.0)
     model.clearThreshold
     model.predict
   }
@@ -40,6 +54,24 @@ class LibLinAlg(
   }
 
 }
+
+
+
+object LibLinAlgSerializer extends UnderlyingAlgorithmSerializer[LibLinAlg]{
+  override def serialize(alg: LibLinAlg): String = {
+    alg.getModel.intercept + "\n" +
+    alg.getModel.weights.toArray.map(_.toString).reduce(_+"\n"+_)
+  }
+  override def deserialize(modelString: String): LibLinAlg ={
+    val rowSplitted= modelString.split("\n").map(_.toDouble);
+    val intercept = rowSplitted.head;
+    val weights = rowSplitted.tail;
+    val model = new SVMModel(Vectors.dense(weights), intercept);
+    new LibLinAlg(model);
+  }
+}
+
+
 
 object LIBLINEAR {
   def vectorToFeatures(v: Vector) = {
@@ -75,5 +107,81 @@ object LIBLINEAR {
     Linear.train(problem, parameter)
 
   }
+  
+  case class LibLinParams(
+    calibrationSize: Int = 16,
+    nICPs: Int = 30,
+    solverType: SolverType = SolverType.L2R_L2LOSS_SVC_DUAL,
+    regParam: Double = 1,
+    tol: Double = 0.01)
+  
+  def trainAggregatedClassifier(params: LibLinParams, training_data: RDD[LabeledPoint], sc: SparkContext): AggregatedICPClassifier[LibLinAlg] = {
+
+    //Train icps
+    val trainBroadcast = sc.broadcast(training_data.collect)
+    val icps = sc.parallelize((1 to params.nICPs)).map { _ =>
+      //Sample calibration
+      val shuffData = Random.shuffle(trainBroadcast.value.toList)
+      val positives = shuffData.filter { p => p.label == 1.0 }
+      val negatives = shuffData.filter { p => p.label != 1.0 }
+      val calibration = (
+        positives.take(params.calibrationSize) ++
+        negatives.take(params.calibrationSize))
+        .toArray
+      val properTraining = (
+        negatives.takeRight(negatives.length - params.calibrationSize) ++
+        positives.takeRight(positives.length - params.calibrationSize))
+        .toArray
+      //Train ICP
+      val alg = new LibLinAlg(
+        properTraining,
+        params.solverType,
+        params.regParam,
+        params.tol)
+      ICP.trainClassifier(alg, numClasses = 2, calibration)
+    }
+
+    //Aggregate ICPs 
+    return new AggregatedICPClassifier(icps.collect)
+  }
+  
+  def takeFractions[T](list: List[T], fractions: Array[Double]): Array[List[T]] ={
+    if(fractions.length == 0){
+      return Array(list);
+    }
+    val sumFractions = fractions.sum;
+    val numRecords = list.size;
+    var shuffData = Random.shuffle(list);
+    
+    var lists = fractions.map { frac => 
+      val (currFraction, new_shuffData) = shuffData.splitAt((frac*numRecords/sumFractions).toInt); 
+      /*println(currFraction + ", " + new_shuffData);*/ 
+      shuffData=new_shuffData; currFraction }
+    
+    assert(shuffData.size <= 1)
+    
+    // Give the last item to the biggest fraction (might be false??)
+    val biggestIndex = fractions.indexOf(fractions.min);
+    lists(biggestIndex) = lists(biggestIndex) ++ shuffData;
+    
+    lists
+    
+  }
 
 }
+
+    
+    /*
+    //find smallest partition, and add it to that partition:
+    var index=List(0);
+    var currShortest = Int.MaxValue;
+    
+    for( i <- (0 to lists.length-1)) {
+      if(lists(i).size < currShortest){
+        currShortest = lists(i).size;
+        index = List(i);
+      }
+      else if(lists(i).size == currShortest){
+        index = i::index;
+      }
+    }*/

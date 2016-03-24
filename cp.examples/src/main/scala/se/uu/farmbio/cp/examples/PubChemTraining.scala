f8b6cfcb952aef0331deffa6e82c04a7d61b0004
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-03-24 12:10:15 +0100

    Revert "set regularization parameter to 0"
    This reverts commit 4a1ac4142b8156b69ea20a1b80f70e0605ef0f84.

--- cp.examples/src/main/scala/se/uu/farmbio/cp/examples/PubChemTraining.scala
+++ cp.examples/src/main/scala/se/uu/farmbio/cp/examples/PubChemTraining.scala
@@ -23,7 +23,7 @@ object PubChemTraining {
 
     //Init Spark
     val conf = new SparkConf()
-      .setAppName("PubChemTraining")
+      .setAppName("ICPRunningTime")
     if (params.master != null) {
       conf.setMaster(params.master)
     }
@@ -40,7 +40,7 @@ object PubChemTraining {
         params.clSize,
         stratified = true)
       ICP.trainClassifier(
-        new SVM(training,regParam=0.0),
+        new SVM(training),
         numClasses = 2,
         calibration)
     }
